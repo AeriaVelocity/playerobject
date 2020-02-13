aa756c189593f564523606852975599bd8c70da6
Astrid <sonicspeed848@gmail.com>
2020-02-13 11:50:31 +0000

    Added the ability to fully heal in DefaultPlayer.py

--- DefaultPlayer.py
+++ DefaultPlayer.py
@@ -1,9 +1,11 @@
 import PlayerModule
+import random
 
 PlayerName = ""
 while PlayerName == "":
     PlayerName = input("Please input your name!\n> ")
-Player = PlayerModule.PlayerObject(PlayerName, 100, 15, "Player OOP Test")
+
+Player = PlayerModule.PlayerObject(PlayerName, random.randint(80, 1000), random.randint(15, 300), "Player OOP Test")
 print("Type 'attack' to attack, 'damage' to take damage or 'heal' to regain health.")
     
 while True:
@@ -14,7 +16,10 @@ while True:
         damageamount = int(input("How much base damage do you want to receive?\n> "))
         Player.Damage(damageamount)
     if PlayerInput == "heal":
-        healamount = int(input("How much health do you want to receive?\n> "))
-        Player.Heal(healamount)
+        healamount = input("How much health do you want to receive? (Type 'full' to fully heal)\n> ")
+        if healamount == 'full':
+            Player.Heal(Player.MaxHealth)
+        else:
+            Player.Heal(healamount)
     else:
         pass
